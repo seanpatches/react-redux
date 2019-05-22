437837c287a966227644ac7bcf4b6b4b99633741
Sean Nelson <smnelsonn@gmail.com>
2019-05-22 15:23:07 -0700

    add, remove, array, remove from array

--- src/index.js
+++ src/index.js
@@ -1,8 +1,90 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import { createStore } from 'redux';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+const initialState = {
+  drink: null,
+  chips: null,
+  sandwichIngredients: [],
+};
+
+function reducer(state = initialState, action){
+  switch(action.type){
+    case 'ADD_DRINK':
+      return { ...state, drink: action.payload };
+  
+    case 'ADD_CHIPS':
+      return { ...state, chips: action.payload };
+  
+    case 'ADD_LETTUCE':
+      return { ...state, sandwichIngredients: [...state.sandwichIngredients, action.payload] };
+
+    case 'ADD_TOMATO':
+      return { ...state, sandwichIngredients: [...state.sandwichIngredients, action.payload] };
+
+    case 'REMOVE_DRINK':
+      return { ...state, drink: null };
+  
+    case 'REMOVE_CHIPS':
+      return { ...state, chips: null };
+  
+    case 'REMOVE_INGREDIENT':
+      return { ...state, sandwichIngredients: state.sandwichIngredients.filter((ingredient) => ingredient !== action.payload) };
+
+    case 'REMOVE_ALL':
+      return { ...state, sandwichIngredients: null, drinks: null, chips: null };
+
+    default:
+      state;
+  }
+}
+
+const Store = createStore(reducer);
+
+Store.dispatch({
+  type: 'ADD_DRINK',
+  payload: 'tequila'
+});
+console.log('+drink', Store.getState());
+
+Store.dispatch({
+  type: 'ADD_CHIPS',
+  payload: 'Lays'
+});
+console.log('+chips', Store.getState());
+
+Store.dispatch({
+  type: 'ADD_LETTUCE',
+  payload: 'Lettuce'
+});
+console.log('+let', Store.getState());
+
+Store.dispatch({
+  type: 'ADD_TOMATO',
+  payload: 'Tomato'
+});
+console.log('+tom', Store.getState());
+
+
+Store.dispatch({
+  type: 'REMOVE_DRINK',
+  payload: null
+});
+console.log('-drink', Store.getState());
+
+Store.dispatch({
+  type: 'REMOVE_CHIPS',
+  payload: null
+});
+console.log('-chips', Store.getState());
+
+Store.dispatch({
+  type: 'REMOVE_INGREDIENT',
+  payload: 'Tomato'
+});
+console.log('-all', Store.getState());
+
+Store.dispatch({
+  type: 'REMOVE_ALL',
+  payload: null
+});
+console.log('-all', Store.getState());
+
