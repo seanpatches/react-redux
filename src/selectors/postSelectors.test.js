4d5fec8b1e972b807c36adb3bba9f68909e88661
Sean Nelson <smnelsonn@gmail.com>
2019-05-23 13:16:26 -0700

    selectors working and tested

--- src/selectors/postSelectors.test.js
+++ src/selectors/postSelectors.test.js
@@ -1,4 +1,4 @@
-import { getPosts } from './postSelectors';
+import { getPosts, getPost } from './postSelectors';
 
 describe('post selector', () => {
   it('obtains all posts', () => {
@@ -13,5 +13,18 @@ describe('post selector', () => {
       { title: 'test1', body:'test1', id: 0 },
       { title: 'test2', body:'test2', id: 1 }
     ]);
+  });
+
+  it('obtains a single post by id', () => {
+    const state = {
+      posts: [
+        { title: 'test1', body:'test1' },
+        { title: 'test2', body:'test2' }
+      ]
+    };
+
+    expect(getPost(state, 1)).toEqual(
+      { title: 'test2', body:'test2', id: 1 }
+    );
   });
 });
