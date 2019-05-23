45cd7e609ee437724250d119b92a43c455693706
Sean Nelson <smnelsonn@gmail.com>
2019-05-23 10:55:23 -0700

    added devtools

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import { createStore } from 'redux';
 import reducer from './reducers/index.js';
 
-const store = createStore(reducer);
-
-store.subscribe(() => {
-  console.log(store.getState());
-});
+const store = createStore(reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 store.dispatch({
   type: 'ADD_POST',
